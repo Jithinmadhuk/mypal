d2dc7a3ecafcde96e91c4a175edcffcdd16d26a0
Jithin <jithinmadhu0106@gmail.com>
2026-02-25 16:07:56 +0530

    Added signup view

--- mypal_project/mypal_app/views.py
+++ mypal_project/mypal_app/views.py
@@ -1,2 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import SignUpForm, HomePageForm, TaskForm, DeckForm, FlashcardForm, TimerForm, CalenderForm
+from .models import SignUp, HomePage, Task, Deck, Flashcard, Timer, Calender
+
+def signupView(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home_page')
+        else:
+            form = SignUpForm()
+        return render(request, 'signup_page.html', {'form': form})
 
